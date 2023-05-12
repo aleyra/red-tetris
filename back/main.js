2d88030b6a78b30f200d7ebf1a12118073d666eb
Tactoe <eotcat@gmail.com>
2023-05-12 07:18:59 +0200

    Added player malus, full line deletion

--- back/main.js
+++ back/main.js
@@ -9,9 +9,10 @@ function getKeyPress(ref)
     // listen to keypress
      
     process.stdin.on("keypress", (str, key) => {
-        if(key.name == "a") ref.currentPiece.x -= 1;
-        if(key.name == "d") ref.currentPiece.x += 1;
+        if(key.name == "a") ref.movePieceHorizontally(-1);
+        if(key.name == "d") ref.movePieceHorizontally(1);
         if(key.name == "s") ref.slamPiece();
+        if(key.name == "n") ref.addPlayerMalusNextTurn = true;
         if(key.ctrl && key.name == "c") process.exit(0);
         ref.printGrid();
     });
@@ -31,24 +32,70 @@ function Piece(type) {
     this.type = type;
 }
 
+function Player()
+{
+    this.hasMalus = false;
+}
+
 function Game()
 {
     this.playerCount = 0;
     this.id = totalGames + 1;
 }
 
+
+
 function Grid() {
     this.boardWidth = 10;
     this.boardHeight = 20;
+    this.addPlayerMalusNextTurn = false;
     this.currentPiece = new Piece("O");
     this.grid = Array(this.boardHeight).fill().map(() => Array(this.boardWidth).fill().map(() => "X"));
-    this.placePiece = function placePiece()
+    this.movePieceHorizontally = (direction) =>
+    {
+        this.currentPiece.x += direction;
+    }
+    this.checkForFullLine = () =>
+    {
+        for (var y = this.boardHeight - 1; y >= 0; y--)
+        {
+            var lineIsFull = true;
+            for (var x = 0; x < this.boardWidth; x++)
+            {
+                if (this.grid[y][x] == "X" || this.grid[y][x] == "N")
+                {
+                    lineIsFull = false;
+                    break;
+                }
+            }
+
+            // move all upper row
+            if (lineIsFull)
+            {
+                var previousRow = Array.from(this.grid[0]);
+                for (var y2 = 1; y2 <= y; y2++)
+                {
+                    var tmp = Array.from(this.grid[y2]);
+                    this.grid[y2] = previousRow;
+                    // this.grid[y2] = Array(this.boardWidth).fill().map(() => "W");
+                    previousRow = Array.from(tmp);
+                }
+            }
+        }
+    }
+    this.placePiece = () =>
     {
         this.grid[this.currentPiece.y][this.currentPiece.x] = this.currentPiece.type;
+        this.checkForFullLine();
+        if (this.addPlayerMalusNextTurn)
+        {
+            this.addPlayerMalus();
+            this.addPlayerMalusNextTurn = false;
+        }
         this.currentPiece = new Piece("O");
         this.printGrid();
     }
-    this.slamPiece = function slamPiece() {
+    this.slamPiece = () => {
         var y = this.currentPiece.y;
         var x = this.currentPiece.x;
         while (y + 1 < this.boardHeight && this.grid[y + 1][x] == "X")
@@ -56,10 +103,9 @@ function Grid() {
             y += 1;
         }
         this.currentPiece.y = y;
-        this.currentPiece.x = x;
         this.placePiece();
     }
-    this.checkForCollision = function checkForCollision()
+    this.checkForCollision = () =>
     {
         if (this.currentPiece.y + 1 >= this.boardHeight
             || this.grid[this.currentPiece.y + 1][this.currentPiece.x] != "X")
@@ -67,7 +113,25 @@ function Grid() {
         else
             return true;
     }
-    this.printGrid = function printGrid()
+    this.addPlayerMalus = () =>
+    {
+        for (var x = 0; x < this.boardWidth; x++)
+        {
+            if (this.grid[0][x] != "X")
+                this.gameOver();
+        }
+        // move all rows up to insert the null row
+        var previousRow = Array.from(this.grid[this.boardHeight - 1]);
+        for (var y = this.boardHeight - 2; y >= 0; y--)
+        {
+            var tmp = Array.from(this.grid[y]);
+            this.grid[y] = previousRow;
+            previousRow = Array.from(tmp);
+        }
+
+        this.grid[this.boardHeight - 1] = Array(this.boardWidth).fill().map(() => "N"); 
+    }
+    this.printGrid = () =>
     {
         console.log('\033[2J');
         for (var y = 0; y < this.boardHeight; y++)
@@ -82,6 +146,14 @@ function Grid() {
             console.log("");
         }
     }
+    this.updateGame = () =>
+    {
+        if (this.checkForCollision())
+            this.currentPiece.y += 1;
+        else
+            this.placePiece();
+        this.printGrid();
+    }
     totalGames += 1;
 }
 
@@ -91,10 +163,6 @@ var g = new Grid();
 getKeyPress(g);
 
 function updateGame() {
-    if (g.checkForCollision())
-        g.currentPiece.y += 1;
-    else
-        g.placePiece();
-    g.printGrid();
+    g.updateGame()
 }
 setInterval(updateGame, 1000);
