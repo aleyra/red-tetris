97ad54eeaafa67af86551b424f7636aa49eb7b06
lburnet <lburnet@student.42lyon.fr>
2023-05-16 14:45:26 +0200

    Next_tetrimino a terter, mais comment ?

--- front/src/containers/play/next_tetrimino.jsx
+++ front/src/containers/play/next_tetrimino.jsx
@@ -3,7 +3,7 @@ import React from "react";
 //css
 import './../../css/Play.css'
 
-export function Next_tetrimino_demo(){
+function CaseI(){
 	return(
 		<div className="next-tetrimino-grid">
 			<div className="grid-row">
@@ -14,15 +14,15 @@ export function Next_tetrimino_demo(){
 					<div className="grid-empty-square"></div>
 				</div>
 				<div className="grid-column">
-					<div className="grid-empty-square"></div>
-					<div className="grid-tetriminoT"></div>
-					<div className="grid-tetriminoT"></div>
-					<div className="grid-tetriminoT"></div>
+					<div className="grid-tetriminoI"></div>
+					<div className="grid-tetriminoI"></div>
+					<div className="grid-tetriminoI"></div>
+					<div className="grid-tetriminoI"></div>
 				</div>
 				<div className="grid-column">
 					<div className="grid-empty-square"></div>
 					<div className="grid-empty-square"></div>
-					<div className="grid-tetriminoT"></div>
+					<div className="grid-empty-square"></div>
 					<div className="grid-empty-square"></div>
 				</div>
 				<div className="grid-column">
@@ -34,4 +34,271 @@ export function Next_tetrimino_demo(){
 			</div>
 		</div>
 	);
+}
+
+function CaseJ(){
+	return(
+		<div className="next-tetrimino-grid">
+			<div className="grid-row">
+				<div className="grid-column">
+					<div className="grid-empty-square"></div>
+					<div className="grid-empty-square"></div>
+					<div className="grid-empty-square"></div>
+					<div className="grid-empty-square"></div>
+				</div>
+				<div className="grid-column">
+					<div className="grid-empty-square"></div>
+					<div className="grid-empty-square"></div>
+					<div className="grid-tetriminoJ"></div>
+					<div className="grid-empty-square"></div>
+				</div>
+				<div className="grid-column">
+					<div className="grid-empty-square"></div>
+					<div className="grid-empty-square"></div>
+					<div className="grid-tetriminoJ"></div>
+					<div className="grid-empty-square"></div>
+				</div>
+				<div className="grid-column">
+					<div className="grid-empty-square"></div>
+					<div className="grid-tetriminoJ"></div>
+					<div className="grid-tetriminoJ"></div>
+					<div className="grid-empty-square"></div>
+				</div>
+			</div>
+		</div>
+	);
+}
+
+function CaseL(){
+	return(
+		<div className="next-tetrimino-grid">
+			<div className="grid-row">
+				<div className="grid-column">
+					<div className="grid-empty-square"></div>
+					<div className="grid-empty-square"></div>
+					<div className="grid-empty-square"></div>
+					<div className="grid-empty-square"></div>
+				</div>
+				<div className="grid-column">
+					<div className="grid-empty-square"></div>
+					<div className="grid-tetriminoL"></div>
+					<div className="grid-empty-square"></div>
+					<div className="grid-empty-square"></div>
+				</div>
+				<div className="grid-column">
+					<div className="grid-empty-square"></div>
+					<div className="grid-tetriminoL"></div>
+					<div className="grid-empty-square"></div>
+					<div className="grid-empty-square"></div>
+				</div>
+				<div className="grid-column">
+					<div className="grid-empty-square"></div>
+					<div className="grid-tetriminoL"></div>
+					<div className="grid-tetriminoL"></div>
+					<div className="grid-empty-square"></div>
+				</div>
+			</div>
+		</div>
+	);
+}
+
+function CaseO(){
+	return(
+		<div className="next-tetrimino-grid">
+			<div className="grid-row">
+				<div className="grid-column">
+					<div className="grid-empty-square"></div>
+					<div className="grid-empty-square"></div>
+					<div className="grid-empty-square"></div>
+					<div className="grid-empty-square"></div>
+				</div>
+				<div className="grid-column">
+					<div className="grid-empty-square"></div>
+					<div className="grid-tetriminoO"></div>
+					<div className="grid-tetriminoO"></div>
+					<div className="grid-empty-square"></div>
+				</div>
+				<div className="grid-column">
+					<div className="grid-empty-square"></div>
+					<div className="grid-tetriminoO"></div>
+					<div className="grid-tetriminoO"></div>
+					<div className="grid-empty-square"></div>
+				</div>
+				<div className="grid-column">
+					<div className="grid-empty-square"></div>
+					<div className="grid-empty-square"></div>
+					<div className="grid-empty-square"></div>
+					<div className="grid-empty-square"></div>
+				</div>
+			</div>
+		</div>
+	);
+}
+
+function CaseS(){
+	return(
+		<div className="next-tetrimino-grid">
+			<div className="grid-row">
+				<div className="grid-column">
+					<div className="grid-empty-square"></div>
+					<div className="grid-empty-square"></div>
+					<div className="grid-empty-square"></div>
+					<div className="grid-empty-square"></div>
+				</div>
+				<div className="grid-column">
+					<div className="grid-empty-square"></div>
+					<div className="grid-tetriminoS"></div>
+					<div className="grid-tetriminoS"></div>
+					<div className="grid-empty-square"></div>
+				</div>
+				<div className="grid-column">
+					<div className="grid-tetriminoS"></div>
+					<div className="grid-tetriminoS"></div>
+					<div className="grid-empty-square"></div>
+					<div className="grid-empty-square"></div>
+				</div>
+				<div className="grid-column">
+					<div className="grid-empty-square"></div>
+					<div className="grid-empty-square"></div>
+					<div className="grid-empty-square"></div>
+					<div className="grid-empty-square"></div>
+				</div>
+			</div>
+		</div>
+	);
+}
+
+function CaseT(){
+	return(
+		<div className="next-tetrimino-grid">
+			<div className="grid-row">
+				<div className="grid-column">
+					<div className="grid-empty-square"></div>
+					<div className="grid-empty-square"></div>
+					<div className="grid-empty-square"></div>
+					<div className="grid-empty-square"></div>
+				</div>
+				<div className="grid-column">
+					<div className="grid-empty-square"></div>
+					<div className="grid-empty-square"></div>
+					<div className="grid-tetriminoT"></div>
+					<div className="grid-empty-square"></div>
+				</div>
+				<div className="grid-column">
+					<div className="grid-empty-square"></div>
+					<div className="grid-tetriminoT"></div>
+					<div className="grid-tetriminoT"></div>
+					<div className="grid-tetriminoT"></div>
+				</div>
+				<div className="grid-column">
+					<div className="grid-empty-square"></div>
+					<div className="grid-empty-square"></div>
+					<div className="grid-empty-square"></div>
+					<div className="grid-empty-square"></div>
+				</div>
+			</div>
+		</div>
+	);
+}
+
+function CaseZ(){
+	return(
+		<div className="next-tetrimino-grid">
+			<div className="grid-row">
+				<div className="grid-column">
+					<div className="grid-empty-square"></div>
+					<div className="grid-empty-square"></div>
+					<div className="grid-empty-square"></div>
+					<div className="grid-empty-square"></div>
+				</div>
+				<div className="grid-column">
+					<div className="grid-tetriminoZ"></div>
+					<div className="grid-tetriminoZ"></div>
+					<div className="grid-empty-square"></div>
+					<div className="grid-empty-square"></div>
+				</div>
+				<div className="grid-column">
+					<div className="grid-empty-square"></div>
+					<div className="grid-tetriminoZ"></div>
+					<div className="grid-tetriminoZ"></div>
+					<div className="grid-empty-square"></div>
+				</div>
+				<div className="grid-column">
+					<div className="grid-empty-square"></div>
+					<div className="grid-empty-square"></div>
+					<div className="grid-empty-square"></div>
+					<div className="grid-empty-square"></div>
+				</div>
+			</div>
+		</div>
+	);
+}
+
+function CaseDefault(){
+	return(
+		<div className="next-tetrimino-grid">
+			<div className="grid-row">
+				<div className="grid-column">
+					<div className="grid-empty-square"></div>
+					<div className="grid-empty-square"></div>
+					<div className="grid-empty-square"></div>
+					<div className="grid-empty-square"></div>
+				</div>
+				<div className="grid-column">
+					<div className="grid-empty-square"></div>
+					<div className="grid-empty-square"></div>
+					<div className="grid-empty-square"></div>
+					<div className="grid-empty-square"></div>
+				</div>
+				<div className="grid-column">
+					<div className="grid-empty-square"></div>
+					<div className="grid-empty-square"></div>
+					<div className="grid-empty-square"></div>
+					<div className="grid-empty-square"></div>
+				</div>
+				<div className="grid-column">
+					<div className="grid-empty-square"></div>
+					<div className="grid-empty-square"></div>
+					<div className="grid-empty-square"></div>
+					<div className="grid-empty-square"></div>
+				</div>
+			</div>
+		</div>
+	);
+}
+
+//comment appeler Next_tetrimino avec un paramètre... ?
+export function Next_tetrimino(next_one){
+	if (next_one == "I")
+		return (
+			<CaseI />
+		);
+	else if (next_one == "J")
+		return (
+			<CaseJ />
+		);
+	else if (next_one == "L")
+		return (
+			<CaseL />
+		);
+	else if (next_one == "O")
+		return (
+			<CaseO />
+		);
+	else if (next_one == "S")
+		return (
+			<CaseS />
+		);
+	else if (next_one == "T")
+		return (
+			<CaseT />
+		);
+	else if (next_one == "Z")
+		return (
+			<CaseZ />
+		);
+	else
+		return(
+			<CaseDefault />
+		);
 }
