a07f56f873b8862d1d61f5019244742b29d05a20
lburnet <lburnet@student.42lyon.fr>
2023-05-16 15:30:58 +0200

    un peu de deco

--- front/src/containers/home/Home.jsx
+++ front/src/containers/home/Home.jsx
@@ -2,27 +2,48 @@ import React from "react";
 import { Link } from 'react-router-dom';
 
 //css
-
+import '../../css/Home.css'
 //const
 
 //function
 import { Presentation } from "./Presentation";
+import { OrangeColoredButton, PurpleColoredButton } from "../../components/buttons";
 
 export function Home() {
 	return(
 		<React.Fragment>
-			<div>
-				<Presentation />
-			</div>
-			<div>
-				<Link to={"/SelectMode"}>
-					Select mode to play
-				</Link>
+			<div className="Menu">
+				<div className="button">
+					<Link to={"/SelectMode"}>
+						<PurpleColoredButton
+							variant="contained"
+							disableRipple
+							// sx={{ color: 'black'}}
+						>
+							Select mode to play
+						</PurpleColoredButton>
+					</Link>
+				</div>
+				<div className="button">
+					{/* <Link to={"/Ranking"}> */}
+						<OrangeColoredButton
+							variant="contained"
+							disableRipple
+							// sx={{ color: 'black'}}
+						>
+							Ranking
+						</OrangeColoredButton>
+					{/* </Link> */}
+				</div>
+
 			</div>
 			<div>
 				{/* <Link to={""}> */}
 					Ranking
 				{/* </Link> */}
+			</div>
+			<div>
+				<Presentation />
 			</div>
 		</React.Fragment>
 	);
