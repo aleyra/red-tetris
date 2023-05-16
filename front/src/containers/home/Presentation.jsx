b596fa8abfbcd31c2620227d1dfba38f372e5267
lburnet <lburnet@student.42lyon.fr>
2023-05-16 15:53:58 +0200

    on etoffe la presentation

--- front/src/containers/home/Presentation.jsx
+++ front/src/containers/home/Presentation.jsx
@@ -3,6 +3,10 @@ import React from 'react'
 //css
 import './../../css/Presentation.css';
 
+//images
+import ReactLogo from '../../images/react.png';
+import ReduxLogo from '../../images/redux.png';
+
 export function Presentation(){
 	return(
 		<div>
@@ -17,7 +21,10 @@ export function Presentation(){
 					<div className='Presentation-container-div'>
 						<a href='https://profile.intra.42.fr/users/tmilon'>
 							<div>
-								<img src="https://cdn.intra.42.fr/users/0db64d827e57902861c104f0955ae742/tmilon.jpg" alt={"tlafay's avatar"} className='Presentation-avatar'></img>
+								<img
+									src="https://cdn.intra.42.fr/users/0db64d827e57902861c104f0955ae742/tmilon.jpg"
+									alt={"tlafay's avatar"}
+									className='Presentation-avatar'></img>
 							</div>
 							<div className='Presentation-Name-status'>
 								<div className='Presentation-name'>tmilon</div>
@@ -27,10 +34,47 @@ export function Presentation(){
 					<div className='Presentation-container-div'>
 						<a href='https://profile.intra.42.fr/users/lburnet'>
 							<div>
-								<img src="https://cdn.intra.42.fr/users/554ea290ae9a8010d1b07010036acbce/lburnet.jpg" alt={"lburnet's avatar"} className='Presentation-avatar'></img>
+								<img
+									src="https://cdn.intra.42.fr/users/554ea290ae9a8010d1b07010036acbce/lburnet.jpg"
+									alt={"lburnet's avatar"}
+									className='Presentation-avatar'
+								></img>
 							</div>
 							<div className='Presentation-Name-status'>
 								<div className='Presentation-name'>lburnet</div>
+							</div>
+						</a>
+					</div>
+				</div>
+				<div>
+					Les technologies utilisées sont :
+				</div>
+				<div className='techno-used'>
+					<div className='one-techno'>
+						<a href='https://react.dev/'>
+							<div>
+								<img 
+									src={ReactLogo}
+									alt='React Logo'
+									className='presentation-logo'
+								></img>
+							</div>
+							<div>
+								React
+							</div>
+						</a>
+					</div>
+					<div className='one-techno'>
+						<a href='https://redux.js.org/'>
+							<div>
+								<img
+									src={ReduxLogo}
+									alt='Redux Logo'
+									className='presentation-logo'
+								></img>
+							</div>
+							<div>
+								Redux
 							</div>
 						</a>
 					</div>
