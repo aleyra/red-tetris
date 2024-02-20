4df126d02b170e7b39b030174f7ed26b921591c5
mfunyu <mfunyu@student.42lyon.fr>
2024-02-20 12:30:21 +0100

    add: road index.html file 1/3 #2

--- server/server.js
+++ server/server.js
@@ -1,12 +1,16 @@
 import express from 'express';
 import { createServer } from 'node:http';
+import { fileURLToPath } from 'node:url';
+import { dirname, join } from 'node:path';
 
 const app = express();
 const server = createServer(app);
 const port = 8000;
 
+const __dirname = dirname(fileURLToPath(import.meta.url));
+
 app.get('/', (req, res) => {
-  res.send('<h1>Hello world</h1>');
+  res.sendFile(join(__dirname, 'index.html'));
 });
 
 server.listen(port, () => {
