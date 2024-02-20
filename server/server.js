c9903818f764340253045873f43d6d0c1bc7667c
mfunyu <mfunyu@student.42lyon.fr>
2024-02-20 12:34:35 +0100

    add: setup socket.io 2/3 #2

--- server/server.js
+++ server/server.js
@@ -2,9 +2,11 @@ import express from 'express';
 import { createServer } from 'node:http';
 import { fileURLToPath } from 'node:url';
 import { dirname, join } from 'node:path';
+import { Server } from 'socket.io';
 
 const app = express();
 const server = createServer(app);
+const io = new Server(server);
 const port = 8000;
 
 const __dirname = dirname(fileURLToPath(import.meta.url));
@@ -12,6 +14,13 @@ const __dirname = dirname(fileURLToPath(import.meta.url));
 app.get('/', (req, res) => {
   res.sendFile(join(__dirname, 'index.html'));
 });
+
+  io.on('connection', (socket) => {
+	console.log('a user connected');
+	socket.on('disconnect', () => {
+	  console.log('user disconnected');
+	});
+  });
 
 server.listen(port, () => {
   console.log(`server running at http://localhost:${port}`);
