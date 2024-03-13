bf67d2326983037d687948f6d91faa90a4741dfd
mfunyu <mfunyu@student.42lyon.fr>
2024-03-13 11:30:05 +0100

    update: server.js adapt dev/prod

--- server/server.js
+++ server/server.js
@@ -3,23 +3,47 @@ import { createServer } from 'node:http';
 import { Server } from 'socket.io';
 import config from './config.js';
 
-const app = express();
-const server = createServer(app);
-const io = new Server(server);
-
-app.use(express.static(config.DIR_PATH));
-
-app.get('/', (req, res) => {
-  res.sendFile(config.FILE_PATH);
-});
-
-io.on('connection', (socket) => {
-  console.log('a user connected');
-  socket.on('disconnect', () => {
-    console.log('user disconnected');
+if (config.NODE_ENV == 'production') {
+  const app = express();
+  app.use(express.static(config.DIR_PATH));
+  app.get('/', (req, res) => {
+    res.sendFile(config.FILE_PATH);
   });
-});
 
-server.listen(config.PORT, () => {
-  console.log(`server running at http://localhost:${config.PORT}`);
-});
+  const server = createServer(app);
+  const io = new Server(server);
+
+  io.on('connection', (socket) => {
+    console.log('a user connected');
+    socket.on('disconnect', () => {
+      console.log('user disconnected');
+    });
+  });
+
+  server.listen(config.PORT, () => {
+    console.log(`production server running at http://localhost:${config.PORT}`);
+  });
+}
+else if (config.NODE_ENV == 'development') {
+  const server = createServer();
+  const io = new Server(server, {
+    cors: {
+      origin: 'http://localhost:3000',
+      methods: ['GET', 'POST']
+    }
+  });
+
+  io.on('connection', (socket) => {
+    console.log('a user connected');
+    socket.on('disconnect', () => {
+      console.log('user disconnected');
+    });
+  });
+
+  server.listen(config.PORT, () => {
+    console.log(`development server running at http://localhost:${config.PORT}`);
+  });
+}
+else {
+  console.log('Error: NODE_ENV not set');
+}
