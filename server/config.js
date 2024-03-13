fe121e60885e966daef1118e26889f48d15c7734
mfunyu <mfunyu@student.42lyon.fr>
2024-03-13 11:30:05 +0100

    update: server.js adapt dev/prod

--- server/config.js
+++ server/config.js
@@ -9,6 +9,7 @@ const file_path = path.join(dir_path, process.env.FILENAME || 'index.html');
 
 const config = {
   PORT: process.env.PORT || 8000,
+  NODE_ENV: process.env.NODE_ENV || 'development',
   DIR_PATH: dir_path,
   FILE_PATH: file_path
 };
