2c8604d8c01ea31828d0183a79edf33ea9dd345d
mfunyu <mfunyu@student.42tokyo.jp>
2024-03-18 16:59:40 +0000

    add: createBoard

--- server/src/game/game.gateway.ts
+++ server/src/game/game.gateway.ts
@@ -1,15 +1,19 @@
 import { SubscribeMessage, WebSocketGateway, WebSocketServer } from '@nestjs/websockets';
+import { GameService } from './game.service';
 
 @WebSocketGateway({cors: '*'})
 export class GameGateway {
+  gameService: GameService;
 
   handleConnection() {
+    this.gameService = new GameService();
     console.log('Client connected');
   }
 
-  @SubscribeMessage('gameStart')
+  @SubscribeMessage('startGame')
   handleMessage(client: any, payload: any): string {
     console.log('gameStart called')
+    this.gameService.gameStart();
     return 'Let the game begin!'
   }
 }
