0b5ba61ebe3d1e5cb99359e715c41bfae2af2f60
lburnet <lburnet@student.42lyon.fr>
2024-03-25 17:19:28 +0100

    React Redux Tutorials - 27 : done

--- front/src/components/tuto_react_redux/ItemContainer.js
+++ front/src/components/tuto_react_redux/ItemContainer.js
@@ -1,10 +1,12 @@
 import React from 'react'
 import { connect } from 'react-redux'
+import { buyCake, buyIceCream } from '../../redux'
 
 function ItemContainer(props) {
 	return (
 		<div>
 			<h2>Item - {props.item} </h2>
+			<button onClick={props.buyItem}>Buy Items</button>
 		</div>
 	)
 }
@@ -19,4 +21,17 @@ const mapStateToProps = (state, ownProps) => { //ownProps is the props of the co
 		}
 }
 
-export default connect(mapStateToProps)(ItemContainer)
+const mapDispatchToProps = (dispatch, ownProps) => {
+	const dispatchFunction = ownProps.cake
+		? () => dispatch(buyCake())
+		: () => dispatch(buyIceCream())
+
+		return {
+			buyItem: dispatchFunction
+		}
+}
+
+export default connect(
+	mapStateToProps, //null instead of mapStateToProps if only want to dipatch without subscribe to the store
+	mapDispatchToProps
+)(ItemContainer)
