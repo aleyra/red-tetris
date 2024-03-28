4320f6d9d9781f1bca9a2a9782e4f060d1cf9d4c
mfunyu <mfunyu@student.42lyon.fr>
2024-03-28 13:56:00 +0000

    update: SocketServer to class

--- server/index.ts
+++ server/index.ts
@@ -4,55 +4,51 @@ import { Server as SocketIoServer } from 'socket.io';
 import config from './config';
 
 class SocketServer {
-  private readonly server: SocketIoServer;
+  private is_production: boolean;
+  private readonly server;
   private readonly io: SocketIoServer;
 
   constructor() {
     if (config.NODE_ENV == 'production') {
+      this.is_production = true;
+    } else {
+      this.is_production = false;
+    }
+    if (this.is_production) {
       const app = express();
       app.use(express.static(config.DIR_PATH));
       app.get('/', (req, res) => {
         res.sendFile(config.FILE_PATH);
       });
+      this.server = createServer(app);
+      this.io = new SocketIoServer(this.server);
+    } else {
+      this.server = createServer();
+      this.io = new SocketIoServer(this.server, {
+        cors: {
+          origin: 'http://localhost:3000',
+          methods: ['GET', 'POST']
+        }
+      });
     }
-
   }
 
-}
-
-const server = createServer(app);
-const io: SocketIoServer = new SocketIoServer(server);
-
-io.on('connection', (socket) => {
-  console.log('a user connected');
-  socket.on('disconnect', () => {
-    console.log('user disconnected');
-  });
-});
-
-server.listen(config.PORT, () => {
-  console.log(`production server running at http://localhost:${config.PORT}`);
-});
-if (config.NODE_ENV == 'development') {
-  const server = createServer();
-  const io: SocketIoServer = new SocketIoServer(server, {
-    cors: {
-      origin: 'http://localhost:3000',
-      methods: ['GET', 'POST']
-    }
-  });
-
-  io.on('connection', (socket) => {
-    console.log('a user connected');
-    socket.on('disconnect', () => {
-      console.log('user disconnected');
+  startListening() {
+    this.io.on('connection', (socket) => {
+      console.log('a user connected');
+      socket.on('disconnect', () => {
+        console.log('user disconnected');
+      });
     });
-  });
+    this.server.listen(config.PORT, () => {
+      if (this.is_production)
+        console.log(`production server running at http://localhost:${config.PORT}`);
+      else
+        console.log(`development server running at http://localhost:${config.PORT}`);
+    });
+  }
+}
 
-  server.listen(config.PORT, () => {
-    console.log(`development server running at http://localhost:${config.PORT}`);
-  });
-}
-else {
-  console.log('Error: NODE_ENV not set');
-}
+
+const socketServer = new SocketServer();
+socketServer.startListening();
