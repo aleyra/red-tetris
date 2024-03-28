7fe7f0bb2becd6c901b3f9b351b55733342f69d2
mfunyu <mfunyu@student.42lyon.fr>
2024-03-28 13:45:59 +0000

    add: eslint

--- server/index.ts
+++ server/index.ts
@@ -3,28 +3,37 @@ import { createServer } from 'node:http';
 import { Server as SocketIoServer } from 'socket.io';
 import config from './config';
 
-if (config.NODE_ENV == 'production') {
-  const app = express();
-  app.use(express.static(config.DIR_PATH));
-  app.get('/', (req, res) => {
-    res.sendFile(config.FILE_PATH);
-  });
+class SocketServer {
+  private readonly server: SocketIoServer;
+  private readonly io: SocketIoServer;
 
-  const server = createServer(app);
-  const io: SocketIoServer = new SocketIoServer(server);
+  constructor() {
+    if (config.NODE_ENV == 'production') {
+      const app = express();
+      app.use(express.static(config.DIR_PATH));
+      app.get('/', (req, res) => {
+        res.sendFile(config.FILE_PATH);
+      });
+    }
 
-  io.on('connection', (socket) => {
-    console.log('a user connected');
-    socket.on('disconnect', () => {
-      console.log('user disconnected');
-    });
-  });
+  }
 
-  server.listen(config.PORT, () => {
-    console.log(`production server running at http://localhost:${config.PORT}`);
-  });
 }
-else if (config.NODE_ENV == 'development') {
+
+const server = createServer(app);
+const io: SocketIoServer = new SocketIoServer(server);
+
+io.on('connection', (socket) => {
+  console.log('a user connected');
+  socket.on('disconnect', () => {
+    console.log('user disconnected');
+  });
+});
+
+server.listen(config.PORT, () => {
+  console.log(`production server running at http://localhost:${config.PORT}`);
+});
+if (config.NODE_ENV == 'development') {
   const server = createServer();
   const io: SocketIoServer = new SocketIoServer(server, {
     cors: {
